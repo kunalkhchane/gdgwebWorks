4db5f2f309c864c6fe9794d160c3807a37037407
Kunal Khachane <83391782+kunalkhchane@users.noreply.github.com>
2025-03-07 21:56:42 +0530

    Update model.js

--- backend/model.js
+++ backend/model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const formSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String,
+    number: String,
     query: String,
 });
 
